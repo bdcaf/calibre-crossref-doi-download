e615fb2649b1744468aa432ed9cbc752d2554004
Clemens Ager <clemens.ager@gmail.com>
2020-09-24 11:54:35 +0200

    fix module name

--- helper/debug.py
+++ helper/debug.py
@@ -5,16 +5,16 @@ import json
 log = Log()
 mydb = db('~/Archive/CalTest/').new_api
 
-from calibre_plugins.doi_meta import DoiMeta
+from calibre_plugins.crossref_doi_download import DoiMeta
 from calibre.ebooks.metadata.book.base import Metadata
-from calibre_plugins.doi_meta.doi_reader import DoiReader
+from calibre_plugins.crossref_doi_download.doi_reader import DoiReader
 
 dm = DoiMeta('./plugin/')
 
 # url = 'https://api.crossref.org/works/10.1002/bmc.835'
 url = 'https://api.crossref.org/works/10.1109/tpami.2013.50'
 # output = urlopen(url).read()
-qurl='https://api.crossref.org/works?query.author=Bogus%C5%82aw+Buszewski&query.bibliographic=Human+exhaled+air+analytics%3A+biomarkers+of+diseases' 
+qurl='https://api.crossref.org/works?query.author=Bogus%C5%82aw+Buszewski&query.bibliographic=Human+exhaled+air+analytics%3A+biomarkers+of+diseases'
 br = dm.browser
 # cdata = br.open_novisit(qurl).read()
 # d2 =  json.loads(cdata)
@@ -25,4 +25,3 @@ reader = DoiReader(log)
 
 from calibre import ipython
 ipython(locals())
-
